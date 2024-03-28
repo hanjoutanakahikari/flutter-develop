969af31a87d0c6e395ea403102dbd64840cf3d71
hanjoutanakahikari <tanakahikari@hanjou.co.jp>
2024-03-28 17:06:40 +0900

    translated系アニメーション

--- lib/study_animation.dart
+++ lib/study_animation.dart
@@ -1,3 +1,4 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,25 +31,71 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  bool _flag = false;
+class _MyHomePageState extends State<MyHomePage>  with TickerProviderStateMixin {
+    late AnimationController _animationController;
+    late Animation<double> _animationDouble;
+    final Tween<double> _tweenDouble = Tween(begin:0.0, end:200.0);
+    late Animation<Color?> _animationColor;
+    final ColorTween _tweenColor = ColorTween(begin: Colors.deepOrange, end: Colors.white12);
+    late Animation _animation;
 
-  void _click() async{
-    setState(() {
-      _flag = !_flag;
-    });
-  }
+    //再生
+    void _forward() async{
+      setState(() {
+        _animationController.forward();
+      });
+    }
 
-  void _incrementCounter() {
-    setState(() {
-      
-      _counter++;
-    });
-  }
+    //停止
+    _stop() async{
+      setState(() {
+        _animationController.stop();
+      });
+    }
 
-  @override
-  Widget build(BuildContext context) {
+    //逆再生
+    void _reverse() async{
+      setState(() {
+        _animationController.reverse();
+      });
+    }
+
+    @override
+    void initState(){
+      super.initState();
+      //アニメーションの更新や同期を行うためのController
+      _animationController = AnimationController(
+        vsync: this,
+        duration: const Duration(seconds: 3)
+      );
+      //Tween+AnimationContoroller で animationサイズ
+      _animationDouble = _tweenDouble.animate(_animationController);
+      _animationDouble.addListener(() {
+        setState(() {  
+        });
+      });
+      //Tween+AnimationContoroller で animation色
+      _animationColor = _tweenColor.animate(_animationController);
+      _animationColor.addListener(() {
+        setState(() {
+          
+        });
+      });
+
+      _animation = _animationController.drive(
+        Tween(begin:0.0, end:2.0* pi)
+      );
+
+    }
+
+    @override
+    void dispose(){
+      _animationController.dispose();
+      super.dispose();
+    }
+
+    @override
+    Widget build(BuildContext context) {
     
     return Scaffold(
       appBar: AppBar(
@@ -62,57 +109,52 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            //薄くなるやつ
-            AnimatedOpacity(
-              opacity: _flag ? 0.1 : 1.0, 
-              duration: Duration(seconds: 3),
-              child: Text(
-                "消える文字",
-                style: Theme.of(context).textTheme.headlineLarge
-              )
-            ),
-            //大きさ変わるやつ
-            AnimatedSize(
-              duration: const Duration(seconds: 3) ,
+           Text("AnimationController:${_animationController.value}"),
+           Text("AnimationDouble:${_animationDouble.value}"),
+           Text("AnimationColor:${_animationColor.value}"),
+           //拡大縮小動くやつ
+           SizeTransition(
+            sizeFactor: _animationController,
+            child: Center(
               child: SizedBox(
-                width: _flag ? 50 : 200,
-                height: _flag ? 50 : 200,
-                child: Container(color: Colors.deepPurple),
-              )
+                width: _animationDouble.value,
+                height: _animationDouble.value,
+                child: Container(
+                  color: _animationColor.value,
+                )
+              ),
+            )
             ),
-            //斜めに動くやつ
-            AnimatedAlign(
-              duration: const Duration(seconds: 3),
-              alignment: _flag ? Alignment.topLeft : Alignment.bottomRight,
-              child: SizedBox(
-                width: 50,
-                height: 50,
-                child: Container(color: Colors.brown),
-              )
-            ),
-            //変形するやつ
-            AnimatedContainer(
-              duration: const Duration(seconds: 3),
-              width: _flag ? 100 : 50,
-              height: _flag ? 50 : 100,
-              padding: _flag ? const EdgeInsets.all(0) : const EdgeInsets.all(30),
-              margin: _flag ? const EdgeInsets.all(0) : const EdgeInsets.all(30),
-              transform: _flag ? Matrix4.skewX(0.0) : Matrix4.skewX(0.3),
-              color: _flag ? Colors.blue :Colors.grey
-            ),
-            //姿が変わるやつ
-            AnimatedSwitcher(
-                duration: const Duration(seconds: 3),
-                child: _flag ? const Text("なにもない") : const Icon(Icons.favorite, color:Colors.pink)
-            ),
-
+            AnimatedBuilder(
+              animation: _animation, 
+              builder: (context,_){
+                return Transform.rotate(
+                  angle: _animation.value,
+                  child: const Icon(Icons.cached, size: 100),
+                );
+              }
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _click,
-        child: const Icon(Icons.add),
-      ), 
+      //再生、停止、逆再生のボタン　
+      floatingActionButton: Row( 
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          FloatingActionButton(
+            onPressed: _forward,
+            child: const Icon(Icons.arrow_circle_up_outlined),
+          ),
+          FloatingActionButton(
+            onPressed: _stop,
+            child: const Icon(Icons.pause),
+          ),
+          FloatingActionButton(
+            onPressed: _reverse,
+            child: const Icon(Icons.arrow_circle_down_outlined)
+          )
+
+        ],), 
     );
   }
 }
